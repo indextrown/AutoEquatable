8070f812d97b91f79fbf362ca0581bba7fe09421
indextrown <indextrown@gmail.com>
2026-01-19 20:20:19 +0900

    chore: package 수정

--- Package.swift
+++ Package.swift
@@ -9,14 +9,10 @@ let package = Package(
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        
-        // 런타임 라이브러리
         .library(
             name: "AutoEquatable",
-            targets: ["AutoEquatable"],
+            targets: ["AutoEquatable"]
         ),
-        
-        // 예제용 실행 파일
         .executable(
             name: "AutoEquatableClient",
             targets: ["AutoEquatableClient"]
@@ -29,10 +25,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         // Macro implementation that performs the source transformation of a macro.
-        
-        // =========================
-        // 1️⃣ Macro 타겟 (컴파일 타임 전용)
-        // =========================
         .macro(
             name: "AutoEquatableMacros",
             dependencies: [
@@ -42,31 +34,16 @@ let package = Package(
         ),
 
         // MARK: - Public Library
-        // =========================
-        // 2️⃣ 런타임 라이브러리
-        // =========================
-        .target(
-            name: "AutoEquatable",
-               dependencies: ["AutoEquatableMacros"]
-           ),
+        .target(name: "AutoEquatable", dependencies: ["AutoEquatableMacros"]),
 
         // MARK: - Client
-        // =========================
-        // 3️⃣ Client (예제)
-        // =========================
-        .executableTarget(
-            name: "AutoEquatableClient",
-            dependencies: ["AutoEquatable"]),
+        .executableTarget(name: "AutoEquatableClient", dependencies: ["AutoEquatable"]),
 
         // MARK: - Tests
-        // =========================
-        // 4️⃣ Tests
-        // =========================
-        .testTarget(
-            name: "AutoEquatableTests",
-                dependencies: [
-                    "AutoEquatableMacros",
-                    .product(name: "SwiftSyntax", package: "swift-syntax"),
-                ])
+        .testTarget(name: "AutoEquatableTests",
+                    dependencies: [
+                        "AutoEquatableMacros",
+                        .product(name: "SwiftSyntax", package: "swift-syntax"),
+                    ])
     ]
 )
