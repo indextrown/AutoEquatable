00d6937780f015f450abc0c06a852805713a2c71
indextrown <indextrown@gmail.com>
2026-01-19 19:36:58 +0900

    chore: package.swift 수정

--- Package.swift
+++ Package.swift
@@ -9,10 +9,14 @@ let package = Package(
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
+        
+        // 런타임 라이브러리
         .library(
             name: "AutoEquatable",
             targets: ["AutoEquatable"]
         ),
+        
+        // 예제용 실행 파일
         .executable(
             name: "AutoEquatableClient",
             targets: ["AutoEquatableClient"]
@@ -25,6 +29,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         // Macro implementation that performs the source transformation of a macro.
+        
+        // =========================
+        // 1️⃣ Macro 타겟 (컴파일 타임 전용)
+        // =========================
         .macro(
             name: "AutoEquatableMacros",
             dependencies: [
@@ -34,12 +42,21 @@ let package = Package(
         ),
 
         // MARK: - Public Library
-        .target(name: "AutoEquatable", dependencies: ["AutoEquatableMacros"]),
+        // =========================
+        // 2️⃣ 런타임 라이브러리
+        // =========================
+        .target(name: "AutoEquatable", dependencies: []),
 
         // MARK: - Client
+        // =========================
+       // 3️⃣ Client (예제)
+       // =========================
         .executableTarget(name: "AutoEquatableClient", dependencies: ["AutoEquatable"]),
 
         // MARK: - Tests
+        // =========================
+        // 4️⃣ Tests
+        // =========================
         .testTarget(name: "AutoEquatableTests",
                     dependencies: [
                         "AutoEquatableMacros",
